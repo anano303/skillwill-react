2fa04de65de0b4f8834cc08c8d81b11d484f1772
anano303 <124179773+anano303@users.noreply.github.com>
2024-01-09 23:41:46 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import PageList from "./components/PageList";
+
+function App() {
+  return (
+    <div className="App">
+      <PageList />
+    </div>
+  );
+}
+
+export default App;
